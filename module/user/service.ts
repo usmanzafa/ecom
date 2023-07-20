a89837ca83a2cae1db4fdfd0a1023a145696b430
Usman Zafar <usmanzafar.mangocoders@gmail.com>
2023-07-20 13:26:50 +0500

    Removed all files that are in the .gitignore

--- module/user/service.ts
+++ module/user/service.ts
@@ -22,14 +22,17 @@ export const comparePassword = async (password, hashedPassword) => {
 // additional function for controller
 
 export const getUsers = () => userModel.find();
+
 export const getUserByEmail = (email: string) => userModel.findOne({ email });
+
 export const getUserBySessionToken = (sessionToken: string) =>
   userModel.findOne({ "authentication.sessionToken": sessionToken });
+
 export const getUserById = (id: string) => userModel.findById(id);
+
 export const createUser = (values: Record<string, any>) =>
   new userModel(values).save().then((user) => user.toObject());
-export const deleteUserById = (id: string) =>
-  userModel.findOneAndDelete({ _id: id });
+
 export const updateUserById = (id: string, values: Record<string, any>) =>
   userModel.findByIdAndUpdate(id, values);
 
