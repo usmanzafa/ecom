b5905b1f9560deb2e2aa19cd7a72ffc6226546f6
Usman Zafar <usmanzafar.mangocoders@gmail.com>
2023-07-24 10:55:25 +0500

    Prehandler Hook Use In routes

--- module/product/index.ts
+++ module/product/index.ts
@@ -2,10 +2,38 @@ import fastify from "fastify";
 import { ProductControllerInstance } from "./controller";
 
 export default (fastify: any, optn: any, done: any) => {
-  fastify.post("/new", ProductControllerInstance.createProduct);
-  fastify.get("/all", ProductControllerInstance.getAllProduct);
-  fastify.get("/getOne/:id", ProductControllerInstance.getOneProduct);
-  fastify.delete("/delete/:id", ProductControllerInstance.deleteProduct);
-  fastify.put("/update/:id", ProductControllerInstance.upDateProduct);
+  // PreHandler hook to be executed before the main route handlers
+  function productPreHandler(request: any, reply: any, done: any) {
+    console.log("Product PreHandler");
+    done();
+  }
+
+  // Define the routes using the controller instance methods with preHandler hook
+  fastify.post(
+    "/new",
+    { preHandler: productPreHandler },
+    ProductControllerInstance.createProduct
+  );
+  fastify.get(
+    "/all",
+    { preHandler: productPreHandler },
+    ProductControllerInstance.getAllProduct
+  );
+  fastify.get(
+    "/getOne/:id",
+    { preHandler: productPreHandler },
+    ProductControllerInstance.getOneProduct
+  );
+  fastify.delete(
+    "/delete/:id",
+    { preHandler: productPreHandler },
+    ProductControllerInstance.deleteProduct
+  );
+  fastify.put(
+    "/update/:id",
+    { preHandler: productPreHandler },
+    ProductControllerInstance.upDateProduct
+  );
+
   done();
 };
